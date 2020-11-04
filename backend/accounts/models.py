0433fd3ee3b384ef43670640148aaf7487ebcaea
a-samir97 <a.samir9710@gmail.com>
2020-11-04 09:23:01 +0200

    added social accounts model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class SocialAccounts(models.Model):
+    facebook = models.URLField()
+    github = models.URLField()
+    twitter = models.URLField()
+    linkedin = models.URLField()
+
+    def __str__(self):
+        return self.facebook
+
 class User(AbstractUser):
     
+    accounts = models.ForeignKey(SocialAccounts, on_delete=models.DO_NOTHING, null=True, blank=True)
     image = models.ImageField()
     following = models.ManyToManyField (
         'self', 
@@ -13,6 +23,7 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+# Intermediate Model for many to mant relation
 class UserFollowing(models.Model):
     user_following = models.ForeignKey(User, related_name='rel_followed', on_delete=models.CASCADE)
     user_followed = models.ForeignKey(User,related_name='rel_following' ,on_delete=models.CASCADE)
