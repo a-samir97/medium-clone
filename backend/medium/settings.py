b124bce2ee5a7c848190ee946b47be2a76c9de86
Ahmed Samir <a.samir9710@gmail.com>
2020-11-12 01:56:29 +0200

    edited secret key in local development

--- backend/medium/settings.py
+++ backend/medium/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from .secrets import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,9 +96,10 @@ if os.getenv('BUILD_ON_TRAVIS', None):
     }
 
 else:    
-    
+
+    from .secrets import KEY
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = KEY
     DEBUG = True
 
     DATABASES = {
