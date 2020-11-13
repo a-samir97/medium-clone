07b942621d9dad79fed62ef481818ef9e28c7658
Ahmed Samir <a.samir9710@gmail.com>
2020-11-13 02:36:33 +0200

    added logout function, token authentication as default authentication instead of session authentiction

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.SignupAPIView.as_view(), name='signup'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('social/', views.SocialAccountsAPIView.as_view(), name='social'),
+    path('logout/', views.LogoutAPIView.as_view(), name='logout')
 ]
