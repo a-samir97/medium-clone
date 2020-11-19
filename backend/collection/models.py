4f367a61d6348210d1c6995e5d02097e0c0da0a1
Ahmed Samir <a.samir9710@gmail.com>
2020-11-20 01:31:34 +0200

    create apps

---
+++ backend/collection/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from accounts.models import User
+from posts.models import Post
+
+class Collection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='collections')
+    name = models.CharField(max_length=30)
+    posts = models.ManyToManyField(Post)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ backend/vote/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from posts.models import Post
+from accounts.models import User
+
+class Upvote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_likes')
+
+    def __str__(self):
+        return self.user.username + " likes " + self.post.title
+
+class Downvote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_unlikes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_unlikes')
+
+    def __str__(self):
+        return self.user.username + " unlikes " + self.post.title
