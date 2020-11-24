b625f52ca73b1b15af007ee26d42755cbffc627c
Ahmed Samir <a.samir9710@gmail.com>
2020-11-25 01:21:27 +0200

    added downvote and upvote for posts

---
+++ backend/vote/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'vote'
+
+urlpatterns = [
+    path('toggle-upvote/<int:post_id>/', views.ToggleUpvoteAPIView.as_view(), name='toggle-upvote'),
+    path('toggle-downvote/<int:post_id>/', views.ToggleDownvoteAPIView.as_view(), name='toggle-downvote'),
+    path('upvote-counts/<int:post_id>/', views.UpvotePostCountsAPIView.as_view(), name='upvote-count'),
+    path('downvote-counts/<int:post_id>/', views.DownvotePostCountsAPIView.as_view(), name='downvote-counts')
+]
--- backend/vote/views.py
+++ backend/vote/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
-# Create your views here.
+from .models import Downvote, Upvote
+from posts.models import Post
+
+class ToggleUpvoteAPIView(APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, post_id):
+        post = Post.objects.filter(id=post_id).first()
+        if post:
+            upvote = Upvote.objects.filter(user=request.user, post=post)
+
+            if upvote:
+                upvote.delete()
+                return Response(
+                    {"data": "upvote removed"},
+                    status=status.HTTP_200_OK
+                )
+            else:
+                Upvote.objects.create(user=request.user, post=post)
+                return Response(
+                    {"data": "upvote added"},
+                    status=status.HTTP_200_OK
+                )
+        else:
+            return Response(
+                {"error": "post is not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+class ToggleDownvoteAPIView(APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, post_id):
+        post = Post.objects.filter(id=post_id).first()
+        if post:
+            downvote = Downvote.objects.filter(user=request.user, post=post)
+
+            if downvote:
+                downvote.delete()
+                return Response(
+                    {"data": "downvote removed"},
+                    status=status.HTTP_200_OK
+                )
+            else:
+                Downvote.objects.create(user=request.user, post=post)
+                return Response(
+                    {"data": "downvote added"},
+                    status=status.HTTP_200_OK
+                )
+        else:
+            return Response(
+                {"error": "post is not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+class UpvotePostCountsAPIView(APIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, post_id):
+        post = Post.objects.filter(id=post_id).first()
+        if post:
+            upvote_count = Upvote.objects.filter(post=post).count()
+            return Response(
+                {"data": upvote_count},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"error": "post is not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+    
+class DownvotePostCountsAPIView(APIView):
+
+    permission_classes =(permissions.AllowAny,)
+
+    def get(self, request, post_id):
+        post = Post.objects.filter(id=post_id).first()
+
+        if post:
+            downvote_count = Downvote.objects.filter(post=post).count()
+            return Response(
+                {"data": downvote_count},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"error": "post is not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
