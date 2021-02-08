3a4e52ba227dfd970604d279bfc06b16f01c48e3
Ahmed Samir <a.samir9710@gmail.com>
2021-02-08 02:04:21 +0200

    Fixed format in post application using flake8 and pep8

--- backend/posts/managers.py
+++ backend/posts/managers.py
@@ -1,23 +1,22 @@
 from django.db.models import Manager
 
+
 class PostManager(Manager):
-    
+
     def get_published_posts(self):
         '''
-            Get all publsihed posts 
+            Get all publsihed posts
         '''
         return self.filter(status='P')
 
-    
     def get_recent_posts(self):
         '''
-            Get the recent 10 posts 
+            Get the recent 10 posts
         '''
         return self.order_by('-publish')[:10]
-    
 
     def get_most_clapped_posts(self):
         '''
-            Get most 10 clapped posts 
+            Get most 10 clapped posts
         '''
         return self.order_by('-clapped')[:10]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import viewsets, permissions, status 
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView
@@ -13,12 +13,16 @@ from .serializers import (
 )
 from .permissions import IsOwner
 
+
 class UserPublishedPostsListAPI(ListAPIView):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = PostSerializer
-    
+
     def get_queryset(self):
-        return Post.posts.get_published_posts().filter(author=self.request.user)
+        return Post.posts.get_published_posts().filter(
+            author=self.request.user
+        )
+
 
 class UserDraftedPostsListAPI(ListAPIView):
     permission_classes = (permissions.IsAuthenticated,)
@@ -27,15 +31,18 @@ class UserDraftedPostsListAPI(ListAPIView):
     def get_queryset(self):
         return Post.objects.filter(status="D", author=self.request.user)
 
+
 class MostClappedPostListAPI(ListAPIView):
     queryset = Post.posts.get_most_clapped_posts()
     serializer_class = PostSerializer
     permission_classes = (permissions.AllowAny,)
 
+
 class LatestPostListAPI(ListAPIView):
     queryset = Post.posts.get_recent_posts()
     serializer_class = PostSerializer
     permission_classes = (permissions.AllowAny,)
+
 
 class ClappPostAPI(APIView):
     permission_classes = (permissions.IsAuthenticated,)
@@ -46,6 +53,7 @@ class ClappPostAPI(APIView):
         post.save()
         post_serializer = PostSerializer(post)
         return Response(post_serializer.data)
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.posts.get_published_posts()
@@ -65,21 +73,27 @@ class PostViewSet(viewsets.ModelViewSet):
             permission_classes = (permissions.IsAuthenticated,)
 
         elif self.action == 'update' or self.action == 'destroy' or \
-            self.action == 'partial_update':
+                self.action == 'partial_update':
             permission_classes = (permissions.IsAuthenticated, IsOwner)
-        
+
         else:
             permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-        
+
         return [permission() for permission in permission_classes]
 
     def create(self, request):
         serializer = PostCreationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
         else:
-            return Response(serializer.errors, satus=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
     def retrieve(self, request, pk=None):
         post = get_object_or_404(Post, pk=pk)
@@ -87,4 +101,3 @@ class PostViewSet(viewsets.ModelViewSet):
         post.save()
         post_serializer = PostSerializer(post)
         return Response(post_serializer.data)
- 
